371a0f4bef4971d45796732761f060666e4c44a3
Isy <isy1997@freenet.de>
2024-02-17 21:39:51 +0100

    Add modals

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,4 @@
+/* Highlight menu items by section on scroll */
 let sections = document.querySelectorAll('section');
 let menuItems = document.querySelectorAll('.navigation-list__item');
 window.onscroll = () => {
@@ -14,3 +15,23 @@ window.onscroll = () => {
         };
     });
 };
+
+/* Modals */
+const openModal = document.querySelector('.open-modal');
+const parentElement = openModal.parentElement;
+const parentHeader = parentElement.querySelector('h2');
+const closeModal = document.querySelector('.close-modal');
+const modal = document.querySelector('.modal');
+const modalHeader = modal.querySelector('h2');
+
+// "More" button open dialog + if statement gleich anfang --> welche openModal // oder extra function // extra const?
+openModal.addEventListener('click', () => {
+    modal.showModal();
+    console.log(parentHeader);
+    console.log(modalHeader);
+});
+
+// "Close" button closes modal
+closeModal.addEventListener('click', () => {
+    modal.close();
+})
